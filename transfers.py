0e09dec6d2137c6aea464bc91a39a0274db1ff65
mikecuoco <mcuoco@ucsd.edu>
2024-11-22 18:46:08 -0800

    customize playlists to transfer

--- transfers.py
+++ transfers.py
@@ -2,33 +2,14 @@
 # THE TRANSFER WILL ONLY WORK IF YOU THE AUTHENTICATED USER IS THE OWNER OF THE TO_PLAYLIST. 
 TRANSFERS = [
   {
-    "from_playlist_name": "New Commute Now",
-    "from_playlist_link": "https://open.spotify.com/playlist/7tOT1UK2vnqTpWbwmuJs1a?si=e29942bd652c42a9",
-    "to_playlist_name": "New Commute 2024 Archive",
-    "to_playlist_link": "https://open.spotify.com/playlist/1KHuoPDQnNWPpW9UDtbZxP?si=426c0f52e0f14cc2",
+    "from_playlist_name": "Discover Weekly",
+    "from_playlist_link": "https://open.spotify.com/playlist/37i9dQZEVXcEyI8HVQqu5P?si=7e543369ac234c8e",
+    "to_playlist_name": "Discovery Weekly Archive",
+    "to_playlist_link": "https://open.spotify.com/playlist/1kz8uNTHyXDZjv8pkiHB3J?si=60ffb8ebe2074c65",
   },
-    {
-    "from_playlist_name": "Pollen",
-    "from_playlist_link": "https://open.spotify.com/playlist/37i9dQZF1DWWBHeXOYZf74?si=460546a70dd74d0f",
-    "to_playlist_name": "Pollen Archive",
-    "to_playlist_link": "https://open.spotify.com/playlist/6WEshQCCEtwL46n7aX8KzA?si=50d72ac9b36e46e1",
-  },
-    {
-    "from_playlist_name": "Compact Cassette",
-    "from_playlist_link": "https://open.spotify.com/playlist/6c6Ra1iI1JjhADV2gnT7p6?si=b8322658ffc34f10",
-    "to_playlist_name": "Compact Cassette Archive",
-    "to_playlist_link": "https://open.spotify.com/playlist/1Z4mPhioxV0U1dMqiCxs2r?si=55c47938ac854674",
-  },
-  {
-    "from_playlist_name": "2024 - Favorite Tracks (So Far)",
-    "from_playlist_link": "https://open.spotify.com/playlist/6Db7AUrNyw93xs9fnZKabQ?si=532714784b524fc0",
-    "to_playlist_name": "2024 Favorite Tracks Archive",
-    "to_playlist_link": "https://open.spotify.com/playlist/1Z4mPhioxV0U1dMqiCxs2r?si=55c47938ac854674",
-  },
-
 ]
 
 DAYLIST = {
-  "sync_on": False,
-  "link": "https://open.spotify.com/playlist/37i9dQZF1EP6YuccBxUcC1?si=2f0d3c7e8c454599"
+  "sync_on": True,
+  "link": "https://open.spotify.com/playlist/37i9dQZF1EP6YuccBxUcC1?si=2e71abc369a44b96"
 }
